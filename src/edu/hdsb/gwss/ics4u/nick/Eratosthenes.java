1a0cef64011ce62bf43710804034d2bf9824c779
Nick <Nick@10.96.177.206>
2014-09-10 08:29:34 -0400

    Start of computer programing

---
+++ src/edu/hdsb/gwss/ics4u/nick/Eratosthenes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.ics4u.nick;
+
+/**
+ *
+ * @author Nick
+ */
+public class Eratosthenes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i;
+        for (i = 1; i < 1001; i++) {
+            
+            System.out.println(i);
+            
+
+        }
+    }
+
+}
