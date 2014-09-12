0a306f828a7f45b913513418fb7e13b4dd441ba4
Nick <Nick@10.96.178.52>
2014-09-12 08:45:17 -0400

    Start of computer programing

---
+++ src/edu/hdsb/gwss/ics4u/nick/Hangman.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.ics4u.nick;
+
+/**
+ *
+ * @author Nick
+ */
+public class Hangman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] category = {
+            "TV Shows", "Movies", "Video Games"};
+        String[] phrase = {
+            "family guy",""};
+
+        } 
+    }
+
