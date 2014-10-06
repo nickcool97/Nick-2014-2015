51a43112d895e047d434f958d7e3b72a0d428d90
Nick <Nick@10.96.176.248>
2014-10-06 09:20:08 -0400

    Start of computer programing

---
+++ src/edu/hdsb/gwss/nick/ics4u/u2/Pacman.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.hdsb.gwss.nick.ics4u.u2;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Nick
+ */
+public class Pacman extends PacmanGame{
+    
+    
+        c.setColor(Color.yellow);
+        c.fillArc(0, 0, PACMAN_RADIUS, PACMAN_RADIUS, 30, 330);
+       
+    
+}
---
+++ src/edu/hdsb/gwss/nick/ics4u/u2/PacmanGame.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.hdsb.gwss.nick.ics4u.u2;
+
+import hsa.Console;
+
+/**
+ *
+ * @author Nick
+ */
+public class PacmanGame {
+     public static void main(String[] args) {
+    Console c = new Console();
+     }
+    
+}
